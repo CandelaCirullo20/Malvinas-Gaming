cb56bb09cba7746b8a5e13cd55d4c55fd6b9a87b
candela.avalos.cirullo <avaloscirullocande@gmail.com>
2023-02-26 23:36:38 -0300

    nav

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import NavBar from './components/Navbar';
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+    return (
+      <NavBar />
+          );
+}
+
+export default App;
